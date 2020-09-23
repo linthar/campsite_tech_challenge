174aaed7df4abfc2b809c66a6e1769177f631f27
Fernando Garcia <fernando.garcia@artekium.com>
2020-09-23 20:06:46 -0300

    Rest API skeleton

---
+++ service/src/main/java/com/upgrade/campsite/Main.java
@@ -0,0 +1,9 @@
+package com.upgrade.campsite;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Main {
+    public static void main(String[] args) {
+        Micronaut.run(args);
+    }
+}
---
+++ service/src/main/java/com/upgrade/campsite/rest/ReservationController.java
@@ -0,0 +1,35 @@
+package com.upgrade.campsite.rest;
+
+import io.micronaut.core.convert.format.Format;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
+
+import javax.annotation.Nullable;
+import java.time.LocalDate;
+
+@Controller("/reservation")
+public class ReservationController {
+
+    @Get()
+    public String availability(@QueryValue @Format("yyyy-MM-dd") @Nullable LocalDate from,
+                               @QueryValue @Format("yyyy-MM-dd") @Nullable LocalDate to) {
+
+
+        // verificar si today sigue siendo == LocalDate.now()
+        LocalDate today = LocalDate.now();
+        if (from == null) {
+            from = today.plusDays(1);
+        }
+
+        if (to == null) {
+            to = today.plusMonths(1);
+        }
+
+        String r = "availability :__" + from + " __to__ " + to;
+
+        return r;
+    }
+
+
+}
